c6bf1e839e771e4c871b81e66e0cf9e00e1db553
SmritiSrivastava08 <83022838+SmritiSrivastava08@users.noreply.github.com>
2021-06-05 08:56:33 +0530

    Update views.py

--- crms/views.py
+++ crms/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.models import User,auth
+from .forms import WriterForm
+from .models import Writer
 
 # Create your views here.
 def home(request):
@@ -27,6 +29,33 @@ def writer_page(request):
 def logout(request):
     auth.logout(request)
     return redirect('home')
+
+def writer_form(request,id=0):
+    if request.method=='GET':
+        if id==0:
+            form=WriterForm()
+        else:
+            writer=Writer.objects.get(pk=id)
+            form=WriterForm(instance=writer)
+        return render(request,'writer_form.html',{'form':form})
+    else:
+        if id==0:
+            form=WriterForm(request.POST)
+        else:
+            writer=Writer.objects.get(pk=id)
+            form=WriterForm(request.POST,instance=writer)
+        if form.is_valid():
+            form.save()
+        return redirect('writer_list')
+
+def writer_delete(request,id):
+    writer=Writer.objects.get(pk=id)
+    writer.delete()
+    return redirect('writer_list')
+
+def writer_list(request):
+    context = {'writer_list':Writer.objects.all()}
+    return render(request,'writer_list.html',context)
 
 def add(request):
     val1=int(request.POST['num1'])
