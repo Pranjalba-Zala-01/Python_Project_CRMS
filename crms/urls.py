db2431843859e3a157de036f00b6db33d17b5892
SmritiSrivastava08 <83022838+SmritiSrivastava08@users.noreply.github.com>
2021-06-05 08:58:18 +0530

    Update urls.py

--- crms/urls.py
+++ crms/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path('login',views.login,name='login'),
     path('writer_page',views.writer_page,name='writer_page'),
     path('logout',views.logout,name='logout'),
+    path('logout',views.logout,name='logout'),
+    path('writer_form',views.writer_form,name='writer_form'),
+    path('writer_form/<int:id>/',views.writer_form,name='writer_update'),
+    path('writer_delete/<int:id>/',views.writer_delete,name='writer_delete'),
+    path('writer_list',views.writer_list,name='writer_list'),
 ]
